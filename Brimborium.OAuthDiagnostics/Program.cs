859cdbe01d16ae387781400ab9f60f5ba6d148e7
Florian Grimm <grimmuko@gmail.com>
2025-02-13 01:17:38 +0100

    create project

---
+++ Brimborium.OAuthDiagnostics/Program.cs
@@ -0,0 +1,35 @@
+namespace Brimborium.OAuthDiagnostics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            /*
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+            */
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapRazorPages()
+               .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
